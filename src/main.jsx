fd90f00d44eed02a02bb7c8d0ae4cbd7a3254dbb
mhung24 <hung123mqtb@gmail.com>
2026-01-09 06:52:43 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import * as PusherPushNotifications from "@pusher/push-notifications-web";
 
-const beamsClient = new PusherPushNotifications.Client({
-  instanceId: "a701f499-cd7c-4320-bd1c-170e7307f91e",
-});
+// const beamsClient = new PusherPushNotifications.Client({
+//   instanceId: "a701f499-cd7c-4320-bd1c-170e7307f91e",
+// });
 
-beamsClient
-  .start()
-  .then(() => beamsClient.addDeviceInterest("hello"))
-  .then(() => console.log("Đăng ký nhận thông báo Beams thành công!"))
-  .catch(console.error);
+// beamsClient
+//   .start()
+//   .then(() => beamsClient.addDeviceInterest("hello"))
+//   .then(() => console.log("Đăng ký nhận thông báo Beams thành công!"))
+//   .catch(console.error);
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
